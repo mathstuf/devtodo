0e19b1e54d30f00703b246f83ebc44c28845dd72
Ben Boeckel <mathstuf@gmail.com>
2020-04-09 23:23:31 -0400

    clap: add basic argument parsing skeleton

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,32 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use clap::{self, App, Arg};
 use human_panic::setup_panic;
+use thiserror::Error;
 
 mod config;
 mod todo;
 
+#[derive(Debug, Error)]
+#[error("setup error")]
+enum SetupError {
+}
+
+fn try_main() -> Result<(), SetupError> {
+    let matches = App::new("devtodo")
+        .version(clap::crate_version!())
+        .author("Ben Boeckel <mathstuf@gmail.com>")
+        .about("Query code hosting platforms for todo items to add to a calendar")
+        .get_matches();
+
+    Ok(())
+}
+
 fn main() {
     setup_panic!();
+
+    if let Err(err) = try_main() {
+        panic!("{:?}", err);
+    }
 }
